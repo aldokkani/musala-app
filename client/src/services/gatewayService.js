0cdbb65faada3b0c84dd3e0fd9df2a7e02f333a2
aldokkani <hossam.aldokkani@gmail.com>
2021-09-12 22:59:34 +0200

    feat(client): create gateway services

---
+++ client/src/services/gatewayService.js
@@ -0,0 +1,45 @@
+const SERVER_URL = 'http://localhost:8000';
+
+export const fetchAllGateways = async () => {
+  const response = await fetch(`${SERVER_URL}/gateways`, {
+    method: 'GET',
+  });
+  return await response.json();
+};
+
+export const updateGateway = async (id, data) => {
+  const response = await fetch(`${SERVER_URL}/gateways/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+};
+
+export const deleteGateway = async (id) => {
+  const response = await fetch(`${SERVER_URL}/gateways/${id}`, {
+    method: 'DELETE',
+  });
+  return await response.json();
+};
+
+export const updateDevice = async (gatewayId, id, data) => {
+  const response = await fetch(
+    `${SERVER_URL}/gateways/${gatewayId}/devices/${id}`,
+    {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    },
+  );
+  return await response.json();
+};
+export const deleteDevice = async (gatewayId, id) => {
+  const response = await fetch(
+    `${SERVER_URL}/gateways/${gatewayId}/devices/${id}`,
+    {
+      method: 'DELETE',
+    },
+  );
+  return await response.json();
+};
