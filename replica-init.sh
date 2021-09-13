a707c249f0e579dd3fac0bdf3e9d02741f879418
aldokkani <hossam.aldokkani@gmail.com>
2021-09-14 00:35:18 +0200

    build(server): setup mongo replica set

---
+++ replica-init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mongo --host mongo0:27017 <<EOF
+rs.initiate({
+    "_id": "rs0",
+    "members": [
+        {
+            "_id": 0,
+            "host": "mongo0:27017",
+        },
+        {
+            "_id": 1,
+            "host": "mongo1:27017",
+        },
+        {
+            "_id": 2,
+            "host": "mongo2:27017",
+        }
+    ]
+});
+EOF
